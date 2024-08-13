8121fd17b72ba83da7a204fa2b91425117f802af
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-08-13 16:25:23 +0300

    Time: 832 ms (5.03%) | Memory: 17 MB (64.25%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,9 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(heights)
+        swapped = False
         for i in range(n-1):
             for j in range(n-1-i):
                 if heights[j] < heights[j+1]:
                     heights[j],heights[j+1] = heights[j+1],heights[j]
                     names[j], names[j+1] = names[j+1], names[j]
+                    swapped = True
+            if (swapped == False):
+                break
         return names
