cd33251becd360bdf5f785d92c99a24939d001d9
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-08-16 16:34:43 +0300

    Time: 126 ms (55.83%) | Memory: 18.2 MB (20.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,8 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        s = 0 
         p = 0
+        s = 0
         while s < len(nums):
             if nums[s] != 0:
                 nums[p], nums[s] = nums[s], nums[p]
