59d61490bd24a7ce8d25077d5471e40189662938
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-08-23 02:39:46 +0300

    Time: 35 ms (68.65%) | Memory: 16.6 MB (21.38%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -7,9 +7,10 @@ class Solution:
         for i in range(len(s)):
             count[s[i]] = count.get(s[i],0) + 1
 
-            if len(count) == 3:
-                goodString += 1
+
             if i >= 2:
+                if len(count) == 3:
+                    goodString += 1                
                 count[s[j]] -= 1
                 if count[s[j]] == 0:
                     del count[s[j]]
