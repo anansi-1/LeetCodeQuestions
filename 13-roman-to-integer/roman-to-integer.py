2cab77505b70dc0394e866a77401c1c55c448408
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-08-30 00:45:06 +0300

    Time: 53 ms (21.97%) | Memory: 16.5 MB (88.38%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,11 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romanNumerals = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        numeralValue = 0
-        for index,value in enumerate(s):
-            if index < len(s) - 1 and romanNumerals.get(value) < romanNumerals.get(s[index+1]):
-                numeralValue -= int(romanNumerals.get(value))
+        d = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sum = 0
+        n = len(s)
+        i = 0
+
+        while i < n:
+            if i < n -1 and d[s[i]] < d[s[i+1]]:
+                sum += d[s[i+1]] - d[s[i]]
+                i += 2
             else:
-                numeralValue += int(romanNumerals.get(value))
-        return numeralValue
-    
+                sum += d[s[i]]
+                i += 1
+        return sum
