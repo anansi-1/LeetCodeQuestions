766a601fc2171fe51d570a37ab1e7c484ef13c1f
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-08-30 01:49:09 +0300

    Time: 32 ms (79.00%) | Memory: 16.6 MB (20.11%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,10 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        j = 0
-        for i in s:
-            found_index = t[j:].find(i)
-            if found_index == -1:
-                return False
-            j += found_index + 1
-            
-        return True
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+
+        
