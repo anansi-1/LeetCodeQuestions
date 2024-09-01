43fe6af05b6f073f229e775b78cafe0d724f9f0a
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-09-01 22:52:43 +0300

    Time: 59 ms (50.06%) | Memory: 16.5 MB (90.65%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,11 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        letters = {}
+        letters = defaultdict(int)
         for letter in magazine:
-            letters[letter] = letters.get(letter, 0) + 1
+            letters[letter] += 1
         for i in ransomNote:
             if i in letters:
-                letters[i] = letters.get(i) - 1
+                letters[i] -= 1
                 if letters.get(i) == 0:
                     del letters[i]
             else:
