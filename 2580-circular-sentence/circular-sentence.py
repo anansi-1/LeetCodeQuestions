47c5832a3702b95fc20ec142fa2915c44ad46449
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-09-08 01:37:49 +0300

    Time: 38 ms (40.13%) | Memory: 16.4 MB (70.50%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -1,11 +1,14 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
-        s = sentence.split()     # "leetcode" "eats" "soul"
-        for i in range(len(s)-1): # 0          1        
-            if s[i][-1] != s[i+1][0]:
-                return False
-        last_word = s[-1]
-        first_word = s[0]
-        if s[-1][-1] != s[0][0]:
-            return False
-        return True
+        s = sentence.split(" ")
+        n = len(s)
+        if n == 1:
+            return sentence[0] == sentence[-1]
+
+        valid = s[0][0] == s[-1][-1]
+        if not valid:
+            return valid
+
+        return valid and all(
+            map(lambda i: s[i][-1] == s[i+1][0], range(n-1))
+        )
