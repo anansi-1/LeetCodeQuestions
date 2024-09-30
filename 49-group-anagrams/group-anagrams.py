8a46eb03c02688afbe2fe5d2f980a4a099cffc81
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-09-30 15:23:01 +0300

    Time: 100 ms (20.14%) | Memory: 22.1 MB (19.09%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,29 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        map = {}
+        res = defaultdict(list)
+        for word in strs:
+            count = [0]*26
+            for letter in word:
+                count[ord(letter)-ord("a")] += 1
+            res[tuple(count)].append(word)
         
-        for str in strs:
-            key = [0] * 26
-
-            for c in str:
-                key[ord(c) - ord('a')] += 1
-
-            keytuple = tuple(key)
-
-            if keytuple in map:
-                map[keytuple].append(str)
-            
-            else:
-                map[keytuple] = [str]
-
-        
-        return map.values()
-
-        
-
-
-
-
-
-        
+        return res.values()
