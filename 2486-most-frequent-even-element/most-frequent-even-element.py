c789b04b1a36675b3af0fb78951da61b55bd43f6
anansi-1 <123372788+anansi-1@users.noreply.github.com>
2024-10-11 12:40:20 +0300

    Time: 214 ms (75.29%) | Memory: 17.2 MB (22.44%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,15 +1,19 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         even = []
-        for n in nums:
-            if n == 0 or n%2 == 0:
-                even.append(n)
-        dict = Counter(even)
+        for number in nums:
+            if number==0 or number%2==0:
+                even.append(number)
+        count = defaultdict(int) #{0:1 , 4:2, 1:1, 2:2}
+        for i in even:
+            count[i] += 1
         frequency = 0
         frequent = -1
-        for n,c in dict.items():
-            if c > frequency or (c==frequency and n<frequent):
-                frequency = c
-                frequent = n
-
+        for num,occur in count.items():
+            if occur > frequency or (occur==frequency and num < frequent):
+                frequency = occur
+                frequent = num
         return frequent
+
+
+
