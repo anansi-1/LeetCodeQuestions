410ffb749671a8ed8017f212d9b83ea56770c571
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-01-30 18:08:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.30%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        empty = numBottles
+        while empty >= numExchange:
+            drinkable = empty // numExchange
+            total += drinkable
+            extra = empty % numExchange
+            empty = drinkable + extra
+        return total
