9cb965a397743ee949500ea9f1d35f620ec196a3
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-02 20:45:37 +0300

    Time: 64 ms (7.25%) | Memory: 17.8 MB (6.42%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            index = i + nums[i]
+            result.append(nums[index % len(nums)])
+        return result
