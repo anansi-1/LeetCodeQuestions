c223539f1dc1989409d15ac2938c89c93f87315c
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-03 18:10:25 +0300

    Time: 1290 ms (12.51%) | Memory: 18.8 MB (29.14%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(max(nums)+1):  
+            if i in nums:
+                continue
+            else:
+                return i
+        return max(nums) + 1
+        
