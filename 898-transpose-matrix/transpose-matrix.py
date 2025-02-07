a7ac0ecfd40afc30009706fc599b4ede75db992d
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-07 07:05:51 +0300

    Time: 3 ms (18.41%) | Memory: 18.5 MB (58.72%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+            rows = len(matrix)
+            cols = len(matrix[0])
+
+            ans = []
+            for i in range(cols):
+                row = []
+                for j in range(rows):
+                    row.append(matrix[j][i])
+                ans.append(row)
+            return ans
