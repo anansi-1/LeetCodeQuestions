d8ac531df1063d4d01f4505726425738c9a19224
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-07 12:23:17 +0300

    Time: 246 ms (11.43%) | Memory: 19.7 MB (6.25%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,29 +1,24 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        directions = [ ]
-        rows = len(img) - 1 # 0 1 2
-        cols = len(img[rows]) # 
-        ans = [[0]*cols for _ in range(len(img))]
-
-        for i in range(rows+1): # 0 1 2
+        directions = []
+        
+        rows = len(img)
+        cols = len(img[0])
+        ans = [[0]*cols for _ in range(rows)]
+        for i in range(rows): # 0 1 2
             for j in range(cols): # 0 1 2
                 directions.append((i,j))
-        # print(directions)
-        def inbound(x,y):
-            return (0<= x < len(img)) and (0<=y < cols)
-        #
-        # img[i][j]
-        # img[i][j-1]
-        # img[i-1][j-1]
-        # img[i-1][j]
-        for i,j in directions:
-            s = 0
-            count = 0   #(0,0)
-            for x in (i-1,i,i+1): 
-                for y in (j-1,j,j+1):
-                        if inbound(x,y):
-                            s += img[x][y]
-                            count += 1
-            ans[i][j] = s//count
         
+        def inbound(x,y):
+            return ((0 <= x < rows) and (0 <= y < cols))
+
+        for i,j in directions:
+            sum =0
+            count = 0
+            for x in (i-1,i,i+1):
+                for y in (j-1,j,j+1):
+                    if inbound(x,y):
+                        sum += img[x][y]
+                        count += 1
+            ans[i][j] = sum//count
         return ans
