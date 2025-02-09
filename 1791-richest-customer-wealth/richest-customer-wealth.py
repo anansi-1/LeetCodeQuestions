eb8e8b115eb322ec395e833583725c238d96fcd4
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-09 13:32:38 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.38%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,6 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        maximum = float("-inf")
-        for i in accounts:
-            maximum = max(maximum,sum(i))
-        return maximum
+        richest = float('-inf')
+        for customer in accounts:
+            wealth = 0            
+            for money in customer:
+                wealth += money
+            richest = max(richest,wealth)
+        return richest
+
+            
