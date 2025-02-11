3c6e459854f1c40c81546f067544bc7df7f46d52
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-11 18:31:55 +0300

    Time: 69 ms (89.17%) | Memory: 27.5 MB (19.57%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = max(nums) 
+        m = min(nums) 
+        multi = (n - m) + 1
+        count = [0] * multi
+        res = []
+        count = [0]*multi
+        for num in nums:
+            count[num- m] += 1
+        for i in range(len(count)):
+            for j in range(count[i]):
+                ans = i + m
+                res.append(ans)
+        return res
