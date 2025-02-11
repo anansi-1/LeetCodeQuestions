ffe21b5cf0f0ff72d760da574274d64a84526de7
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-11 14:32:19 +0300

    Time: 14 ms (33.26%) | Memory: 18.8 MB (90.50%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,10 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        occur = {}
+        for i in s:
+            occur[i] = occur.get(i,0) + 1
+        occur_pair = sorted(occur.items(),key=lambda x:x[1],reverse=True)
+        ans = []
+        for k,v in occur_pair:
+            ans.append(k*v)
+        return "".join(ans)
