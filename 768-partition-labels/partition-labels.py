75bf85a03a7f57c859eb6b261502ac25cd09173b
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-13 19:40:04 +0300

    Time: 4 ms (63.55%) | Memory: 17.6 MB (81.32%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -4,12 +4,12 @@ class Solution:
         for i in range(len(s)-1,-1,-1):
             if s[i] not in last_occur:
                 last_occur[s[i]] = i
-        ans = []
         l = 0
         r = 0
+        ans = []
         for i in range(len(s)):
             r = max(r,last_occur[s[i]])
             if i == r:
-                ans.append(r-l+1)
+                ans.append(r - l + 1)
                 l = i + 1
         return ans
