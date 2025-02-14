7ce191b567d83f918bc94cd11368acab88f7af09
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-02-14 19:18:11 +0300

    Time: 191 ms (45.05%) | Memory: 23.7 MB (16.10%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,19 +1,19 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        container = {}
-        j = 0
-        max_fruits = 0
-
-        for i in range(len(fruits)):
-            container[fruits[i]] = container.get(fruits[i], 0) +1
-            while len(container) > 2:
-                container[fruits[j]] -= 1
-                if container[fruits[j]] == 0:
-                    del container[fruits[j]]
-                j += 1
-            max_fruits = max(max_fruits, i - j +1)
+        cur_count = 0
+        max_fruit = float("-inf")
+        l =0 
+        fruit_count = {}
+        for r in range(len(fruits)):
+            fruit_count[fruits[r]] = fruit_count.get(fruits[r],0)+1
+            cur_count += 1
+            while len(fruit_count) > 2:
+                fruit_count[fruits[l]] -= 1
+                if fruit_count[fruits[l]] == 0:
+                    del fruit_count[fruits[l]]
+                cur_count -= 1
+                l += 1
+            max_fruit = max(cur_count,max_fruit)
         
-        return max_fruits
+        return max_fruit
 
-                    
-        
