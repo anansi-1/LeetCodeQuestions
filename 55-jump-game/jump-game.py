c143ab51e4fbd38d77e4db954f4ff07f783920ca
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-04 09:50:59 +0300

    Time: 42 ms (30.67%) | Memory: 18.6 MB (73.90%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        end = 0
+        for i,num in enumerate(nums):
+            if i > end:
+                return False
+            end = max(end,i+num)
+        return True
