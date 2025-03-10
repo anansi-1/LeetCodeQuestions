65c1e06c107020509a9812b788e0719e3394cbe7
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-10 18:54:04 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.74%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        is_open = False
+        stack = []
+        count = 0
+        for i in s:
+            if i == "(":
+                is_open = True
+                stack.append(i)
+            else:
+                if is_open:
+                    count += 2**(len(stack)-1)
+                    is_open = False
+                stack.pop()
+        return count
