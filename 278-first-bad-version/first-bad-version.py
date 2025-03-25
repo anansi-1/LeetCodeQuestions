3c87d44901951e16eae943ddc190cf0de859edc4
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-25 19:57:13 +0300

    Time: 28 ms (96.30%) | Memory: 17.7 MB (68.08%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        first_bad = 0
+        l,r = 1,n
+        while l <= r:
+            mid = (l + r)//2
+            if isBadVersion(mid):
+                first_bad = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return first_bad
