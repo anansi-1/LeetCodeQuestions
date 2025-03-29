1a20ec9514e1c00fc641a2622a8ce7ce7dd567f5
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-03-30 02:05:40 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.05%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i == ']':
+                char = ''
+                count = ''
+                while stack and stack[-1] != '[':
+                    char = stack.pop() + char
+                stack.pop()
+                while stack and stack[-1].isdigit():
+                    count = stack.pop() + count
+                ans = int(count) * char
+                stack.extend(ans)
+                continue
+            else:
+                stack.append(i)
+        return "".join(stack)
