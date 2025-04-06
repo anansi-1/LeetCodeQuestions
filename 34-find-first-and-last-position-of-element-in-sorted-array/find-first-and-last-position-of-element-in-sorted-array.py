9f758883521709dcb1d9b5796f4fbaf0700404a7
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-04-06 22:38:14 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (52.96%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # starting -> the smallest index equal to x
         starting = -1
+        l,r = 0,len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                starting = mid
+                r = mid -1
+            elif nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
         ending = -1
-        def start(nums,target):
-            nonlocal starting
-            l = 0
-            r = len(nums) - 1
-            while l <= r:
-                mid = (r+l)//2
-                if nums[mid] == target:
-                    starting = mid
-                    if mid == 0:
-                        return starting
-                    l = 0
-                    r = mid - 1
-                elif nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return starting
-        def end(nums,target):
-            nonlocal ending
-            l = 0
-            r = len(nums) - 1
-            while l <= r:
-                mid = (r+l)//2
-                if nums[mid] == target:
-                    ending = mid
-                    if mid == len(nums)-1:
-                        return ending
-                    l = mid + 1
-                    r = len(nums) - 1
-                elif nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return ending
-        return [start(nums,target),end(nums,target)]
+        l,r = 0,len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                ending = mid
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+        return [starting,ending]
