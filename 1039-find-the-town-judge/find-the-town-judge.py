20da7146d8cf9a2206eb56fc527b345c0e9a435a
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-04-10 18:44:58 +0300

    Time: 15 ms (59.11%) | Memory: 20.9 MB (81.20%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int: 
+        if n == 1:
+            return 1
+        indegree = [0]*n
+        outdegree = [0]*n
+        for a,b in trust:
+            indegree[b-1] += 1
+            outdegree[a-1] += 1
+        print(indegree,outdegree)
+        for i in range(n):
+            if indegree[i]==n-1 and outdegree[i] == 0:
+                return i+1
+        return -1
