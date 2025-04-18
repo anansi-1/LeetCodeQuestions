8c4dcc45e3ad5173e26204ee53f24db6405cf4ff
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-04-18 23:18:06 +0300

    Time: 2 ms (40.82%) | Memory: 17.9 MB (49.28%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -0,0 +1,14 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        sorted_deck = sorted(deck)
+        idx = deque(range(len(deck)))
+        res = [0] * len(deck)
+        order = []
+        while idx:
+            curr = idx.popleft()
+            order.append(curr)
+            if idx:
+                idx.append(idx.popleft())
+        for i, idx in enumerate(order):
+            res[idx] = sorted_deck[i]
+        return res
