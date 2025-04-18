2c5c6bf3dd1b6e690380275defbbe8112b06a5d4
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-04-18 22:56:02 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (56.96%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        ans = []
+        def backtrack(path,seen):
+            if len(path) == len(nums):
+                ans.append(path[:])
+                return
+            for i in range(len(nums)):
+                if not seen[i]:
+                    seen[i] = True
+                    path.append(nums[i])
+                    backtrack(path,seen)
+                    path.pop()
+                    seen[i] = False 
+        backtrack([],[False]*len(nums))
+        return ans
