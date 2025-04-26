18247961679d88c0288fdac84369af3ae85763b3
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-04-27 01:14:53 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (88.55%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,19 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-            def dfs(r,c,orginal):
-                image[r][c] = color
-                for i,j in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
-                    if (0 <= i < len(image) and 0 <= j < len(image[0])) and image[i][j] == orginal:
-                        dfs(i,j,orginal)
-            
             if image[sr][sc] == color:
                 return image
-
+    
             orginal = image[sr][sc]
-            image[sr][sc] = color
-            
-            for r,c in [(sr+1,sc),(sr-1,sc),(sr,sc+1),(sr,sc-1)]:
-                if (0 <= r < len(image) and 0 <= c < len(image[0])) and image[r][c] == orginal:
-                    print(orginal)
-                    dfs(r,c,orginal)
+            queue = deque()
+            queue.append((sr,sc))
+
+            while queue:
+                x,y = queue.popleft()
+                image[x][y] = color
+                for r,c in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                    if (0 <= r < len(image) and 0 <= c < len(image[0])) and image[r][c] == orginal:
+                        queue.append((r,c))
             return image
