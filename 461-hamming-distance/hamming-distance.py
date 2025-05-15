f8f924f1e4558fdd7411c0a548793315557b9a6e
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-05-15 18:24:29 +0300

    Time: 1 ms (10.28%) | Memory: 17.9 MB (13.10%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ans = 0
+        r = x ^ y
+        while r:
+            if (r&1):
+                ans += 1
+            r >>= 1
+        return ans
