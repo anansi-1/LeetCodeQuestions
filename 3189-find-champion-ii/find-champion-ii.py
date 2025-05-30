b4bc35b60addcb68632ae6f6112587bfdf98c43f
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-05-30 03:54:43 +0300

    Time: 15 ms (38.20%) | Memory: 18.8 MB (81.55%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        count = defaultdict(int)
+        for u, v in edges:
+            count[v] += 1
+        ans, t = 0, 0
+        for i in range(n):
+            if not count[i]:
+                ans = i
+                t += 1
+            if t == 2:
+                return -1
+        return ans
