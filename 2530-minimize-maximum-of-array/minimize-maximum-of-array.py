5425a490d2ff5a5470dcc790aa18c82a26a8fd61
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-06-07 23:31:07 +0300

    Time: 86 ms (76.26%) | Memory: 27.8 MB (35.25%) - LeetSync

---
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        answer = 0
+        prefix_sum = 0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            curr = math.ceil(prefix_sum / (i + 1))
+            answer = max(answer, curr)
+        return answer
