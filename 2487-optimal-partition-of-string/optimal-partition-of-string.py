c6ea075a68faab3c34084ddd397eee6c11217873
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-06-08 21:39:53 +0300

    Time: 48 ms (66.53%) | Memory: 18.3 MB (17.89%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        c=0
+        st=""
+        for i in range(len(s)):
+            if s[i] in st:
+                c+=1
+                st=s[i]  
+            else:
+                st+=s[i]
+        if st: 
+            c+=1
+        return c
