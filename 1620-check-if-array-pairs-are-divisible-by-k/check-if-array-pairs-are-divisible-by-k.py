b515d9a0d43654930a499eb5be60bc4c80b5c300
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2025-10-06 18:19:44 +0300

    Time: 81 ms (51.89%) | Memory: 37.9 MB (6.53%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        count = defaultdict(int)
+        for i in arr:
+            r = i % k
+            count[r] += 1
+        for r in range(k//2 + 1):
+            if r == 0:
+                if count[r] % 2 != 0:
+                    return False
+            else: 
+                if count[r] != count[k-r]:
+                    return False
+        return True
