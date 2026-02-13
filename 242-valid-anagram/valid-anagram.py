50ad6b36676feb7e38bb76fbf6ad2779457a08ce
Anansi Sime <123372788+anansi-1@users.noreply.github.com>
2026-02-13 12:31:55 +0300

    Time: 12 ms (53.77%) | Memory: 19.3 MB (61.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # if sorted(s)== sorted(t):
-        #     return True
-        # return False
-        s = Counter(s)
-        t = Counter(t)
-        if Counter(s) == Counter(t):
-            return True
-        return False
+        # two words are anagrams of eachother if they contain the same number and type of letter regardless of order
+
+        if len(s) != len(t):
+            return False
+        
+        occurrance_in_s = {}
+        occurrance_in_t = {}
+
+        for i in range(len(s)):
+            occurrance_in_s[s[i]] = occurrance_in_s.get(s[i],0) + 1
+            occurrance_in_t[t[i]] = occurrance_in_t.get(t[i],0) + 1
+
+
+        return occurrance_in_s == occurrance_in_t
